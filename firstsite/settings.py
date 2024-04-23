e5e3f0056756e74d5abcadb1eeb164f51751b11d
Angel M M <angelmm_hup@yahoo.es>
2024-04-23 12:30:02 +0200

    Update settings.py
    Delete

--- firstsite/settings.py
+++ firstsite/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=%ey_mgwqwj_3#=*c0th6=ea8#jm0&m(^5g3j)8dse+)6u9-=w'
+SECRET_KEY = 'zzzzzz' #
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -100,6 +100,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 
 EMAIL_PORT = 587
 
-EMAIL_HOST_USER = 'angelminambresmoro@gmail.com'
+EMAIL_HOST_USER = 'zzzzzzzzzzzz@gmail.com'
 
-EMAIL_HOST_PASSWORD = 'password'
+EMAIL_HOST_PASSWORD = ''
